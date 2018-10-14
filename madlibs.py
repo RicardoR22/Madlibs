be78c755c0baa96640409dc30255fc4c21e2c4ea
Ricardo Rodriguez <rr092293@gmail.com>
2018-10-13 17:13:08 -0700

    Updated to display story before asking for input

--- madlibs.py
+++ madlibs.py
@@ -5,17 +5,30 @@ def user_input(prompt):
 
 def get_user_input():
 
+    story = """
+    Darth {} looked at his master while his {} breathing filled the room.
+    He was told to go {} everything on the planet of {}.
+    He got in his {} and jumped to hyperspace.
+    Shortly before reaching the planet, he dropped out of hyperspace and was attacked by Rebel {}.
+    He {} them off and continued to the planet`s surface.
+    He landed and confronted more opposition, slicing them down with his {}.
+    He used the {} to choke another Rebel, then {} him aside.
+    He finished off all life on the planet with a/an {} laugh.
+    """
+
+    print(story)
+
     name1 = user_input("Type a name ")
     adjective1 = user_input("Type an adjective ")
-    adjective2 = user_input("Type an adjective ")
     verb1 = user_input("Type a verb ")
-    verb_ed1 = user_input("Type a verb ending in 'ed' ")
-    verb_ed2 = user_input("Type a verb ending in 'ed' ")
     silly_word = user_input("Type a silly word ")
     vehicle = user_input("Type a vehicle ")
+    noun_plural = user_input("Type a plural noun ")
+    verb_ed1 = user_input("Type a verb ending in 'ed' ")
     noun1 = user_input("Type a noun ")
     noun2 = user_input("Type a noun ")
-    noun_plural = user_input("Type a plural noun ")
+    verb_ed2 = user_input("Type a verb ending in 'ed' ")
+    adjective2 = user_input("Type an adjective ")
 
     story = """
     Darth {} looked at his master while his {} breathing filled the room.
@@ -27,6 +40,7 @@ def get_user_input():
     He used the {} to choke another Rebel, then {} him aside.
     He finished off all life on the planet with a/an {} laugh.
     """.format(name1, adjective1, verb1, silly_word, vehicle, noun_plural, verb_ed1, noun1, noun2, verb_ed2, adjective2)
+
 
     print(story)
 
